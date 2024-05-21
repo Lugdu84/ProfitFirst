16651989f5c6be85799226e70165b1b0d5bce050
David Grammatico <david.lugdunum@gmail.com>
2024-05-21 16:58:39 +0200

    add new Account UI

--- app/accounts.tsx
+++ app/accounts.tsx
@@ -1,8 +1,16 @@
-import AccountListItem from '@/components/AccountListItem';
 import AccountsList from '@/components/AccountsList';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import { create } from 'react-test-renderer';
 
 export default function AccountsScreen() {
+	const [name, setName] = useState('');
+	const [cap, setCap] = useState('');
+	const [tap, setTap] = useState('');
+
+	const createAccount = () => {
+		console.warn('Create account', name, cap, tap);
+	};
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
@@ -11,18 +19,59 @@ export default function AccountsScreen() {
 				<Text>TAP</Text>
 			</View>
 			<AccountsList />
+			<View style={styles.inputRow}>
+				<TextInput
+					value={name}
+					onChangeText={(text) => setName(text)}
+					placeholder="Name"
+					style={styles.input}
+				/>
+				<TextInput
+					value={cap}
+					keyboardType="number-pad"
+					onChangeText={(text) => setCap(text)}
+					placeholder="CAP %"
+					style={styles.input}
+				/>
+				<TextInput
+					keyboardType="number-pad"
+					value={tap}
+					onChangeText={(text) => setTap(text)}
+					placeholder="TAP %"
+					style={styles.input}
+				/>
+			</View>
+
+			<Button
+				title="Add account"
+				onPress={createAccount}
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		// flex: 1,
 		padding: 10,
 	},
 	header: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		padding: 10,
+	},
+	inputRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 10,
+		backgroundColor: 'white',
+		borderRadius: 10,
+		marginTop: 10,
+	},
+	input: {
+		// flex: 1,
+	},
+	button: {
+		marginTop: 10,
 	},
 });
