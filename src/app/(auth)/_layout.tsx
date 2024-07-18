8d851e4fe210d4e6322e14996d690618635b5d31
David Grammatico <david.lugdunum@gmail.com>
2024-07-18 19:45:23 +0200

    update layout for auth

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,6 +1,12 @@
-import { Stack } from 'expo-router';
+import { useAuth } from '@/providers/AuthProvider';
+import { Redirect, Stack } from 'expo-router';
 
 const AuthLayout = () => {
+	const { isAuthenticated } = useAuth();
+
+	if (isAuthenticated) {
+		return <Redirect href={'/'} />;
+	}
 	return <Stack />;
 };
 
